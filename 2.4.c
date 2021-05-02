9804885296481180b3f172983e6aaa8df6a80255
sarachan <2020859818@isiswa.uitm.edu.my>
2021-05-02 15:04:11 +0000

    modified my code

--- 2.4.c
+++ 2.4.c
@@ -6,7 +6,29 @@
 
 int main(void)
 {
+	char name[20];
 	for (int i=1; i<5; i++)
 	{
 		printf("What is your name:");
+		fgets(name, sizeof(name),stdin);
+
+		pid_t pid = fork();
 		
+		if(pid == 0)
+		{
+			printf("Your name is: ");
+			puts(name);
+			printf("Child process => PPID=%d, PID=%d\n", getppid(), getpid());
+			exit(0);
+		}
+		else
+		{
+			printf("Parent process => PID=%d\n", getpid());
+			printf("Waiting for child process to finish...\n");
+			wait(NULL);
+			printf("Child process finish.\n");
+		}
+	}
+
+return  EXIT_SUCCESS;
+}
